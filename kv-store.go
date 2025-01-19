272d216a07de13befb1040e20bbcdcb8f33330a6
DamirPavlik <damir.pavlik@gmail.com>
2025-01-19 16:03:13 +0100

    feat: add kv store and get, set and del

---
+++ kv-store.go
@@ -0,0 +1,27 @@
+package main
+
+import "sync"
+
+type KeyValueStore struct {
+	store map[string]string
+	mutex sync.RWMutex
+}
+
+func (kvs *KeyValueStore) Get(key string) (string, bool) {
+	kvs.mutex.RLock()
+	defer kvs.mutex.RUnlock()
+	value, exists := kvs.store[key]
+	return value, exists
+}
+
+func (kvs *KeyValueStore) Set(key, value string) {
+	kvs.mutex.Lock()
+	defer kvs.mutex.Unlock()
+	kvs.store[key] = value
+}
+
+func (kvs *KeyValueStore) Del(key string) {
+	kvs.mutex.Lock()
+	defer kvs.mutex.Unlock()
+	delete(kvs.store, key)
+}
