1edeefb80c9562e73a49cbbcbba75072389f6945
DamirPavlik <damir.pavlik@gmail.com>
2025-01-19 16:54:56 +0100

    feat: add persistance

---
+++ persistance.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func (kvs *KeyValueStore) SaveSnapshot(fileName string) error {
+	kvs.mutex.RLock()
+	defer kvs.mutex.RUnlock()
+
+	data, err := json.Marshal(kvs.store)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName, data, 0644)
+}
+
+func (kvs *KeyValueStore) LoadSnapshot(fileName string) error {
+	file, err := os.ReadFile(fileName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+	return json.Unmarshal(file, &kvs.store)
+}
