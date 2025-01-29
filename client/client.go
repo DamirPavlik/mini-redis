3c931b28267649faa4c79d8550965951bc4dd7e4
DamirPavlik <damir.pavlik@gmail.com>
2025-01-29 21:50:54 +0100

    feat: add resp to client

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"net"
@@ -15,14 +16,20 @@ type Client struct {
 func NewClient(address string) (*Client, error) {
 	conn, err := net.Dial("tcp", address)
 	if err != nil {
-		return nil, fmt.Errorf("failed to connect to server :%w", err)
+		return nil, fmt.Errorf("failed to connect to server: %w", err)
 	}
 
 	return &Client{conn: conn}, nil
 }
 
-func (c *Client) sendComamand(command string) (string, error) {
-	_, err := c.conn.Write([]byte(command))
+func (c *Client) sendCommand(command []string) (string, error) {
+	var buffer bytes.Buffer
+	buffer.WriteString(fmt.Sprintf("*%d\r\n", len(command)))
+	for _, arg := range command {
+		buffer.WriteString(fmt.Sprintf("$%d\r\n%s\r\n", len(arg), arg))
+	}
+
+	_, err := c.conn.Write(buffer.Bytes())
 	if err != nil {
 		return "", fmt.Errorf("failed to send command: %w", err)
 	}
@@ -30,7 +37,7 @@ func (c *Client) sendComamand(command string) (string, error) {
 	reader := bufio.NewReader(c.conn)
 	response, err := reader.ReadString('\n')
 	if err != nil {
-		return "", fmt.Errorf("failed to send res: %w", err)
+		return "", fmt.Errorf("failed to receive response: %w", err)
 	}
 
 	return strings.TrimSpace(response), nil
@@ -41,18 +48,50 @@ func (c *Client) Close() error {
 }
 
 func (c *Client) Set(key, value string) (string, error) {
-	command := fmt.Sprintf("SET %s %s\n", key, value)
-	return c.sendComamand(command)
+	return c.sendCommand([]string{"SET", key, value})
 }
 
 func (c *Client) Get(key string) (string, error) {
-	command := fmt.Sprintf("GET %s\n", key)
-	return c.sendComamand(command)
+	return c.sendCommand([]string{"GET", key})
 }
 
 func (c *Client) Del(key string) (string, error) {
-	command := fmt.Sprintf("DEL %s\n", key)
-	return c.sendComamand(command)
+	return c.sendCommand([]string{"DEL", key})
+}
+
+func (c *Client) LPush(key string, values []string) (string, error) {
+	args := append([]string{"LPUSH", key}, values...)
+	return c.sendCommand(args)
+}
+
+func (c *Client) RPush(key string, values []string) (string, error) {
+	args := append([]string{"RPUSH", key}, values...)
+	return c.sendCommand(args)
+}
+
+func (c *Client) SAdd(key string, members []string) (string, error) {
+	args := append([]string{"SADD", key}, members...)
+	return c.sendCommand(args)
+}
+
+func (c *Client) HSet(key, field, value string) (string, error) {
+	return c.sendCommand([]string{"HSET", key, field, value})
+}
+
+func (c *Client) LPop(key string) (string, error) {
+	return c.sendCommand([]string{"LPOP", key})
+}
+
+func (c *Client) RPop(key string) (string, error) {
+	return c.sendCommand([]string{"RPOP", key})
+}
+
+func (c *Client) HGet(key, field string) (string, error) {
+	return c.sendCommand([]string{"HGET", key, field})
+}
+
+func (c *Client) SMembers(key string) (string, error) {
+	return c.sendCommand([]string{"SMEMBERS", key})
 }
 
 func main() {
@@ -74,9 +113,51 @@ func main() {
 	}
 	fmt.Println("GET response:", value)
 
+	resp, err = c.LPush("mylist", []string{"item1", "item2"})
+	if err != nil {
+		log.Fatalf("LPush failed: %v", err)
+	}
+	fmt.Println("LPUSH response:", resp)
+
+	resp, err = c.RPush("mylist", []string{"item3"})
+	if err != nil {
+		log.Fatalf("RPush failed: %v", err)
+	}
+	fmt.Println("RPUSH response:", resp)
+
+	resp, err = c.SAdd("myset", []string{"member1", "member2"})
+	if err != nil {
+		log.Fatalf("SAdd failed: %v", err)
+	}
+	fmt.Println("SADD response:", resp)
+
+	resp, err = c.HSet("myhash", "field1", "value1")
+	if err != nil {
+		log.Fatalf("HSet failed: %v", err)
+	}
+	fmt.Println("HSET response:", resp)
+
+	resp, err = c.LPop("mylist")
+	if err != nil {
+		log.Fatalf("LPop failed: %v", err)
+	}
+	fmt.Println("LPOP response:", resp)
+
+	resp, err = c.RPop("mylist")
+	if err != nil {
+		log.Fatalf("RPop failed: %v", err)
+	}
+	fmt.Println("RPOP response:", resp)
+
 	resp, err = c.Del("name")
 	if err != nil {
 		log.Fatalf("Del failed: %v", err)
 	}
 	fmt.Println("DEL response:", resp)
+
+	resp, err = c.SMembers("myset")
+	if err != nil {
+		log.Fatalf("SMembers failed: %v", err)
+	}
+	fmt.Println("SMEMBERS response:", resp)
 }
